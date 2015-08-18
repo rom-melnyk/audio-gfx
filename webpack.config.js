01130b00e359590d4000d48fd00a19a22322304b
Roman Melnyk <email.rom.melnyk@gmail.com>
2015-08-18 22:27:11 +0200

    Webpack config applied

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        './app/app.es'
+    ],
+    output: {
+        path: __dirname + '/prod',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.es6?$/, exclude: /node_modules/, loader: 'babel-loader'}
+        ]
+    },
+    devtool: 'source-map'
 };
