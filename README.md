9048c8dc7bc8e838cdf61171799604eda032a9c7
Roman Melnyk <email.rom.melnyk@gmail.com>
2018-06-10 13:28:40 +0200

    Moving old app to Angular infrastructure

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Web Audio API + RxJS / TS demo
+# Web Audio API demo
+
+Technologies used: **Angular 6, RxJS, TS.**
 
 ## Run the demo
-1. Run `npm install`.
+
 1. Place one or more audio files into the `demo/audio/` folder.  
    MP3, OGG or OGA files are expected with proper extension.
-1. Run `npm run dev`.
-1. Open [http://localhost:8080/](http://localhost:8080/) in browser.
+1. Run `npm start`. See [README-ANGULAR.md](./README-ANGULAR.md) for more details.
+1. Open [http://localhost:4200/](http://localhost:4200/) in browser.
--- audio-src.js
+++ audio-src.js
@@ -5,15 +5,15 @@ const path = require('path');
 const readdir = promisify(fs.readdir);
 const writeFile = promisify(fs.writeFile);
 
-const AUDIO_DIR = path.join(__dirname, 'demo/audio');
+const AUDIO_DIR = path.join(__dirname, 'dist/audio');
 const JSON_FILE = path.join(AUDIO_DIR, 'audio-src.json');
-const AUDIO_FILE_MASK = /\.(mp3|ogg|oga)$/i;
+const AUDIO_FILE_MASK = /\.(mp3|ogg|oga|wav|wma)$/i;
 
 readdir(AUDIO_DIR)
     .then((files) => {
         const audioFiles = files.filter(f => AUDIO_FILE_MASK.test(f));
         if (audioFiles.length < 1) {
-            throw '[ error ] No audio files found in "demo/audio/"';
+            throw `[ error ] No audio files found in "${AUDIO_DIR}/"`;
         }
         return writeFile(JSON_FILE, JSON.stringify(audioFiles));
     })
