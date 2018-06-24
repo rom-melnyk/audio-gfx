22432c1094906b271da1c0a3b99b478fb3d3a111
Roman <email.rom.melnyk@gmail.com>
2018-06-24 11:59:05 +0200

    add-node behavior fixed (type/name mess)

--- src/app/components/nodes/add-node/add-node.component.ts
+++ src/app/components/nodes/add-node/add-node.component.ts
@@ -2,28 +2,21 @@ import { Component, OnInit, Input } from '@angular/core';
 import { NodeTypes } from '../../../constants';
 import { NodeManagerService } from '../../../services/node-manager/node-manager.service';
 
-class AvailableNode {
-  public name: string;
-
-  constructor(public type: NodeTypes) {
-    this.name = NodeTypes[type];
-  }
-}
-
 @Component({
   selector: 'app-add-node',
   templateUrl: './add-node.component.html',
   styleUrls: ['./add-node.component.scss']
 })
 export class AddNodeComponent implements OnInit {
-  public availableNodes: AvailableNode[] = [];
+  private excludedNodes: NodeTypes[] = [ NodeTypes.AudioSourceNode, NodeTypes.AudioDestinationNode ];
+  public availableNodes: NodeTypes[] = [];
   @Input() index: number;
 
   constructor(private nodeManager: NodeManagerService) {
-    this.availableNodes = <AvailableNode[]>Object.keys(NodeTypes)
+    this.availableNodes = <NodeTypes[]>Object.keys(NodeTypes)
       .sort()
-      .map((type: NodeTypes) => new AvailableNode(type))
-      .filter(({ name }) => (name !== NodeTypes.AudioSourceNode && name !== NodeTypes.AudioDestinationNode));
+      .map((type: NodeTypes) => NodeTypes[type])
+      .filter((type: NodeTypes) => !this.excludedNodes.includes(type));
   }
 
   onSelect(e: Event) {
